bb404d9bdaf555fd702bbb665a81d2cc74d413e3
Crowdbotics <team@crowdbotics.com>
2024-04-17 19:03:19 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "long-frog-47578",
+  url: "https://long-frog-47578.botics.co",
+  api: "https://long-frog-47578.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
